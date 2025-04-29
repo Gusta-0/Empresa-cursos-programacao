47bc5b67c21d85c10ef71009a40d6b27e94d353c
Gustavo Alves <gabs.principal.2005@gmail.com>
2025-04-28 22:14:48 -0300

    Inicialização do projeto

---
+++ src/main/java/com/gustavo/empresa_cursos_programacao/EmpresaCursosProgramacaoApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.empresa_cursos_programacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaCursosProgramacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpresaCursosProgramacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gustavo/empresa_cursos_programacao/EmpresaCursosProgramacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.empresa_cursos_programacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaCursosProgramacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
